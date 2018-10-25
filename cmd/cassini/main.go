c7db3159ed795212c5ed631ac16747c2c8304db1
codelaw1 <codelaw1@gmail.com>
2018-10-25 10:16:21 +0800

    add version function

--- cmd/cassini/main.go
+++ cmd/cassini/main.go
@@ -4,8 +4,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/QOSGroup/cassini/commands"
+	"github.com/QOSGroup/cassini/config"
 	"github.com/QOSGroup/cassini/log"
+	"github.com/QOSGroup/cassini/version"
 )
 
 // 链中继服务主程序，包括正常服务启动和mock 测试运行两种模式
@@ -18,10 +22,17 @@ func main() {
 		commands.NewStartCommand(starter, true),
 		commands.NewMockCommand(mocker, true),
 		commands.NewEventsCommand(events, true),
-		commands.NewTxCommand(txHandler, false))
+		commands.NewTxCommand(txHandler, false),
+		commands.NewVersionCommand(versioner, false))
 
 	if err := root.Execute(); err != nil {
 		log.Error("Exit by error: ", err)
 	}
 	log.Debug("Ok.")
+}
+
+var versioner = func(conf *config.Config) (context.CancelFunc, error) {
+
+	fmt.Println(version.Version)
+	return nil, nil
 }
---
+++ commands/version.go
@@ -0,0 +1,16 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func NewVersionCommand(run Runner, isKeepRunning bool) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Show version info",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return commandRunner(run, isKeepRunning)
+		},
+	}
+	return cmd
+}
