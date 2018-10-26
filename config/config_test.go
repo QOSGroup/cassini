bebcb91f3828792300dc7c3e51e956832737636a
wang <wangfeiping@outlook.com>
2018-10-26 11:21:09 +0800

    修改配置文件；增加Consensus 中继共识算法配置；增加测试用例

--- config/config_test.go
+++ config/config_test.go
@@ -12,13 +12,13 @@ func TestConfigParse(t *testing.T) {
 	    {
 	        "name": "qos",
 	        "rpc": {
-		"listen": "tcp://0.0.0.0:27657"
+		        "nodes": "0.0.0.0:27657"
 	        }
 	    },
 	    {
 	        "name": "qsc",
 	        "rpc": {
-		"listen": "tcp://0.0.0.0:27658"
+		         "nodes": "0.0.0.0:27658"
 	        }
 	    }
 	]
@@ -27,7 +27,22 @@ func TestConfigParse(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int(2), len(conf.Mocks))
 	assert.Equal(t, "qos", conf.Mocks[0].Name)
-	assert.Equal(t, "tcp://0.0.0.0:27658", conf.Mocks[1].RPC.ListenAddress)
+	assert.Equal(t, "0.0.0.0:27658", conf.Mocks[1].RPC.NodeAddress)
+}
+
+func TestLoadConfig(t *testing.T) {
+	conf, err := LoadConfig("./config.conf")
+	assert.NoError(t, err)
+
+	assert.Equal(t, "no", conf.Consensus)
+	assert.Equal(t, "nats://192.168.1.99:4222", conf.Nats)
+	assert.Equal(t, int(2), len(conf.Qscs))
+	assert.Equal(t, "qqs", conf.Qscs[1].Name)
+	assert.Equal(t, "192.168.1.100:26657", conf.Qscs[0].NodeAddress)
+
+	assert.Equal(t, int(2), len(conf.Mocks))
+	assert.Equal(t, "qos", conf.Mocks[0].Name)
+	assert.Equal(t, "0.0.0.0:27657,0.0.0.0:28657", conf.Mocks[1].RPC.NodeAddress)
 }
 
 func TestTransfrom(t *testing.T) {
