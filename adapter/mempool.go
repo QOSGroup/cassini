65caa26389bf8b71623a09d1ed3674a4aad1eeb4
wang <wangfeiping@outlook.com>
2018-11-05 12:50:06 +0800

    just fix a nil bug

--- adapter/mempool.go
+++ adapter/mempool.go
@@ -1,16 +1,11 @@
 package adapter
 
 import (
-	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/mempool"
 )
 
 // NewMempool create a new mempool
 func NewMempool() *mempool.Mempool {
-	mc := &config.MempoolConfig{
-		WalPath:   "./wal/mempool.wal",
-		CacheSize: 1000}
-	pool := mempool.NewMempool(mc, nil, 0)
-	pool.InitWAL()
-	return pool
+
+	return nil
 }
