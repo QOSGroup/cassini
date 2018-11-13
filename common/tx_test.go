44b9d27aedf721be5c64e5630200c0d0e4ff2b65
codelaw1 <codelaw1@gmail.com>
2018-11-13 17:22:52 +0800

    fix test case 'TestSignTxQcp'

--- common/tx_test.go
+++ common/tx_test.go
@@ -1,12 +1,12 @@
 package common
 
 import (
-	"encoding/hex"
 	"strings"
 	"testing"
 
 	motx "github.com/QOSGroup/cassini/mock/tx"
 	"github.com/QOSGroup/cassini/types"
+	//catypes "github.com/QOSGroup/cassini/types"
 	"github.com/QOSGroup/qbase/example/basecoin/app"
 	"github.com/QOSGroup/qbase/qcp"
 	"github.com/stretchr/testify/assert"
@@ -35,19 +35,21 @@ func TestTransform(t *testing.T) {
 
 func TestSignTxQcp(t *testing.T) {
 	tx := motx.NewTxQcpMock("abc", "xyz", 1, 99)
-	pri := "a328891040aa18a9fea8baf6ad2b1502391969324258ec8562163adf4e138eb83e0dbd63b60f3521e8dfd13d533b901aaadaedf345b26d400a0fd5fd65c24f7bf66cbfef81"
+	//pri := "a328891040aa18a9fea8baf6ad2b1502391969324258ec8562163adf4e138eb83e0dbd63b60f3521e8dfd13d533b901aaadaedf345b26d400a0fd5fd65c24f7bf66cbfef81"
+	pri := "t8TKcm7kLkbgxsx28daMDgb5wolMSCifVw2uZNDgXFM7Reelc9iSfiNZfAE+AbXCnVoLHS266D1iVzRYcGeXlA=="
 	cdc := app.MakeCodec()
 	err := SignTxQcp(tx, pri, cdc)
 
 	assert.NoError(t, err)
 
-	pub := "1624de64200f3521e8dfd13d533b901aaadaedf345b26d400a0fd5fd65c24f7bf66cbfef81"
-
-	pubHex, err := hex.DecodeString(pub)
-	assert.NoError(t, err)
-
+	//pub := "1624de64200f3521e8dfd13d533b901aaadaedf345b26d400a0fd5fd65c24f7bf66cbfef81"
+	caHex := "{\"type\": \"tendermint/PubKeyEd25519\",\"value\": \"O0XnpXPYkn4jWXwBPgG1wp1aCx0tuug9Ylc0WHBnl5Q=\"}"
 	var pubkey ed25519.PubKeyEd25519
-	cdc.MustUnmarshalBinaryBare(pubHex, &pubkey)
+	//cdc = catypes.CreateCompleteCodec() //TODO
+	err = cdc.UnmarshalJSON([]byte(caHex), &pubkey)
+
+	//var pubkey ed25519.PubKeyEd25519
+	//cdc.MustUnmarshalBinaryBare(pubHex, &pubkey)
 
 	assert.Equal(t, true, tx.Sig.Pubkey.Equals(pubkey), "!!! Sign Tx error !!!")
 }
