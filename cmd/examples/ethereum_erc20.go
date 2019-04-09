1fab37bc7b5e7569486e12bfc829170af7ce23b4
wang <wangfeiping@outlook.com>
2019-04-09 18:10:54 +0800

    test ethereum contracts rpc

---
+++ cmd/examples/ethereum_erc20.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"github.com/QOSGroup/cassini/adapter/ports/ethereum/token" // for demo
+)
+
+func main() {
+	client, err := ethclient.Dial("https://kovan.infura.io/v3/fb298d4afd444cd5b7c5703b99d51f05")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Golem (GNT) Address
+	tokenAddress := common.HexToAddress("0xEAd5C972Fe8Bbf6f725Ab8A4C7E9d40E15f35241")
+	instance, err := token.NewToken(tokenAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0xb0d2da0f43Cd2E44e4F3a38E24945F0ca0Ea95e2")
+	bal, err := instance.BalanceOf(&bind.CallOpts{}, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name, err := instance.Name(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	symbol, err := instance.Symbol(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decimals, err := instance.Decimals(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("name: %s\n", name)
+	fmt.Printf("symbol: %s\n", symbol)
+	fmt.Printf("decimals: %v\n", decimals)
+
+	fmt.Printf("%s: %s\n", symbol, bal)
+
+	fbal := new(big.Float)
+	fbal.SetString(bal.String())
+	value := new(big.Float).Quo(fbal, big.NewFloat(math.Pow10(int(decimals))))
+
+	fmt.Printf("balance: %f", value)
+}
--- cmd/examples/ethereum_eventlog.go
+++ cmd/examples/ethereum_eventlog.go
@@ -1,15 +1,16 @@
+// +build ignore
+
 package main
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"math/big"
-	"strings"
 
-	store "./contracts" // for demo
+	// store "./contracts" // for demo
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -24,7 +25,7 @@ func main() {
 	contractAddress := common.HexToAddress("0xEAd5C972Fe8Bbf6f725Ab8A4C7E9d40E15f35241")
 	query := ethereum.FilterQuery{
 		FromBlock: big.NewInt(10705633),
-		ToBlock:   big.NewInt(10710306),
+		ToBlock:   big.NewInt(10710975),
 		Addresses: []common.Address{
 			contractAddress,
 		},
@@ -35,26 +36,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	contractAbi, err := abi.JSON(strings.NewReader(string(store.StoreABI)))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// contractAbi, err := abi.JSON(strings.NewReader(string(store.StoreABI)))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	for _, vLog := range logs {
 		fmt.Println("block hash: ", vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
 		fmt.Println("block number: ", vLog.BlockNumber)   // 2394201
 		fmt.Println("tx hash: ", vLog.TxHash.Hex())       // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
-
+		fmt.Println("address: ", vLog.Address.Hex())
 		event := struct {
 			Key   [32]byte
 			Value [32]byte
 		}{}
-		err := contractAbi.Unpack(&event, "ItemSet", vLog.Data)
-		if err != nil {
-			log.Fatal(err)
-		}
+		// err := contractAbi.Unpack(&event, "ItemSet", vLog.Data)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 
-		// fmt.Println("data: ", string(vLog.Data))
+		fmt.Println("data: ", hex.EncodeToString(vLog.Data))
 
 		fmt.Println("key: ", string(event.Key[:]))     // foo
 		fmt.Println("value: ", string(event.Value[:])) // bar
@@ -62,12 +63,19 @@ func main() {
 		var topics [4]string
 		for i := range vLog.Topics {
 			topics[i] = vLog.Topics[i].Hex()
+			fmt.Printf("topic[%d]: %s\n", i, topics[i])
 		}
 
-		fmt.Println("topic: ", topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+		// 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
 	}
 
-	eventSignature := []byte("ItemSet(bytes32,bytes32)")
+	// event Transfer erc20
+	eventSignature := []byte("Transfer(address,address,uint256)")
 	hash := crypto.Keccak256Hash(eventSignature)
-	fmt.Println("hash: ", hash.Hex()) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+	fmt.Println("event hash: ", hash.Hex()) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+
+	eventSignature = []byte("transfer(address,uint256)")
+	hash = crypto.Keccak256Hash(eventSignature)
+	fmt.Println("method id: ", hash.Hex()[0:10])
+
 }
