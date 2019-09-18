0212e2b6176156f3a947db28fc4a155a6343a7c7
wang <wangfeiping@outlook.com>
2019-09-18 09:53:11 +0800

    tag for v0.1.1

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ buildDate=`date +"%F %T %z"`
 goVersion=`go version`
 goVersion=${goVersion#"go version "}
 
-go build --ldflags "-X main.Version=v0.1.0 \
+go build --ldflags "-X main.Version=v0.1.1 \
     -X main.GitCommit=$(git rev-parse HEAD) \
     -X 'main.BuidDate=$buildDate' \
     -X 'main.GoVersion=$goVersion'" \
